6f0ebdaf87442fc91f2588eb0b1878539602f423
Alberto Vallejo <javr1088@gmail.com>
2014-09-09 13:38:18 -0500

    Level 3

--- rubywarrior/vallbert-intermediate/player.rb
+++ rubywarrior/vallbert-intermediate/player.rb
@@ -1,16 +1,66 @@
 class Player
+
+  FIELD = { enemies:[], bound_enemies: [], captives: [], empty: [] }
+
   def play_turn(warrior)
-    direction = warrior.direction_of_stairs
-    watch_battlefield warrior, direction
+    look_around warrior unless @looked
+    evaluate_situation warrior
   end
 
-  def watch_battlefield(warrior, direction)
+  def look_around(warrior)
+    @looked = true
+
+    [:forward, :right, :backward, :left].each do |direction|
+      feeling = warrior.feel direction
+      FIELD[:enemies].push  direction if feeling.enemy?
+      FIELD[:captives].push direction if feeling.captive?
+    end
+  end
+
+  def evaluate_situation(warrior)
+    enemies = FIELD[:enemies].count
+    captives = FIELD[:captives].count
+
+    if enemies > 0
+      neutralize_enemies warrior
+    elsif captives > 0
+      rescue_captives warrior
+    else
+      move_on warrior
+    end
+  end
+
+  def neutralize_enemies(warrior)
+    direction = FIELD[:enemies].pop
+
+    warrior.bind! direction
+    FIELD[:bound_enemies].push direction
+  end
+
+  def rescue_captives(warrior)
+    direction = FIELD[:captives].pop
+    warrior.rescue! direction
+  end
+
+  def move_on(warrior)
+    direction = warrior.direction_of_stairs
     feeling = warrior.feel direction
 
-    if feeling.enemy?
-      warrior.attack! direction
-    else
+    if feeling.empty?
       warrior.walk! direction
+    elsif feeling.enemy?
+      warrior.attack! direction
+    elsif feeling.captive?
+      evaluate_captive warrior, direction
+    end
+  end
+  
+  def evaluate_captive(warrior, direction)
+    if FIELD[:bound_enemies].include?(direction)
+      warrior.attack!(direction)
+      FIELD[:bound_enemies].delete(direction)
+    else
+      warrior.rescue!(direction)
     end
   end
 end
