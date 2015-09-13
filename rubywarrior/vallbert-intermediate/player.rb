4bb5ff3337f1dda7508b54c50c6703cc67c897d9
Alberto Vallejo <javr1088@gmail.com>
2015-09-13 17:08:00 -0500

    Level 2

--- rubywarrior/vallbert-intermediate/player.rb
+++ rubywarrior/vallbert-intermediate/player.rb
@@ -1,6 +1,19 @@
 class Player
+  require 'pry'
+
   def play_turn(warrior)
-    direction = warrior.direction_of_stairs
-    warrior.walk!(direction)
+    @warrior = warrior
+    advance
+  end
+
+  def advance
+    direction = @warrior.direction_of_stairs
+    space = @warrior.feel(direction)
+
+    if space.enemy?
+      @warrior.attack!(direction)
+    else
+      @warrior.walk!(direction)
+    end
   end
 end
